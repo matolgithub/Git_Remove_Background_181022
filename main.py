86d9e78e2e8c8b4909d8948b62d4d44ae5692729
matolgithub <89175955650@mail.ru>
2022-10-18 21:33:49 +0300

    First commit.

---
+++ main.py
@@ -0,0 +1,15 @@
+from rembg import remove
+from PIL import Image
+from pathlib import Path
+
+
+def remove_bg():
+    pass
+
+
+def main():
+    remove_bg()
+
+
+if __name__ == "__main__":
+    main()
